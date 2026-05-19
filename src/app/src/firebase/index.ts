b77facf15867f4f071f2f9a81591cbdd75400192
abdulrhman512 <abdrhmenabdrhmen51@gmail.com>
2026-05-19 19:09:21 +0300

    Initialize Firebase and export Firestore and Auth

---
+++ src/app/src/firebase/index.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "xxx",
+  authDomain: "xxx",
+  projectId: "xxx",
+  storageBucket: "xxx",
+  messagingSenderId: "xxx",
+  appId: "xxx",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+export const firestore = getFirestore(firebaseApp);
+export const auth = getAuth(firebaseApp);
